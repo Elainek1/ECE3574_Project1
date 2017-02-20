67d3bc5987c129c16bf806f3bcad977b8236dce5
Elainek1 <elainek1@vt.edu>
2017-02-20 05:33:48 -0500

    added more unit tests

--- test_interpreter.cpp
+++ test_interpreter.cpp
@@ -9,6 +9,7 @@
 #include "interpreter.hpp"
 #include "expression.hpp"
 #include "test_config.hpp"
+#include "tokenize.hpp"
 
 
 Expression run(const std::string & program){
@@ -63,6 +64,16 @@ TEST_CASE( "Test Interpreter parser with expected input", "[interpreter]" ) {
 
 TEST_CASE( "Test Interpreter parser with truncated input", "[interpreter]" ) {
 
+
+  {
+    std::string program = "(";
+    std::istringstream iss(program);
+  
+    Interpreter interp;
+    bool ok = interp.parse(iss);
+    REQUIRE(ok == false);
+  }
+
   {
     std::string program = "(f";
     std::istringstream iss(program);
@@ -80,6 +91,37 @@ TEST_CASE( "Test Interpreter parser with truncated input", "[interpreter]" ) {
     bool ok = interp.parse(iss);
     REQUIRE(ok == false);
   }
+}
+
+TEST_CASE("Test Interpreter parser with second half input", "[interpreter]") {
+
+
+	{
+		std::string program = ")";
+		std::istringstream iss(program);
+
+		Interpreter interp;
+		bool ok = interp.parse(iss);
+		REQUIRE(ok == false);
+	}
+
+	{
+		std::string program = "f)";
+		std::istringstream iss(program);
+
+		Interpreter interp;
+		bool ok = interp.parse(iss);
+		REQUIRE(ok == false);
+	}
+
+	{
+		std::string program = "gin (define r 10) (* pi (* r r)))";
+		std::istringstream iss(program);
+
+		Interpreter interp;
+		bool ok = interp.parse(iss);
+		REQUIRE(ok == false);
+	}
 }
 
 TEST_CASE( "Test Interpreter parser with extra input", "[interpreter]" ) {
@@ -233,6 +275,96 @@ TEST_CASE( "Test Interpreter special forms: begin and define", "[interpreter]" )
   }
 }
 
+//added cases
+TEST_CASE("Test Interpreter special forms: begin, define, and if", "[interpreter]") {
+
+	{
+		std::string program = "(begin (define a True) (if a 200 100))";
+		Expression result = run(program);
+		REQUIRE(result == Expression(200.));
+	}
+
+	{
+		std::string program = "(begin (define a False) (if a 200 100))";
+		Expression result = run(program);
+		REQUIRE(result == Expression(100.));
+	}
+}
+//added cases
+TEST_CASE("Test Interpreter special forms: begin, define, if, and <", "[interpreter]") {
+
+	{
+		std::string program = "(begin (define a 1) (if (< a 2) 200 100))";
+		Expression result = run(program);
+		REQUIRE(result == Expression(200.));
+	}
+
+	{
+		std::string program = "(begin (define a 1) (if (< a 0) 200 100))";
+		Expression result = run(program);
+		REQUIRE(result == Expression(100.));
+	}
+}
+
+//added cases
+TEST_CASE("Test Interpreter special forms: begin, define, if, and <=", "[interpreter]") {
+
+	{
+		std::string program = "(begin (define a 1) (if (<= a 2) 200 100))";
+		Expression result = run(program);
+		REQUIRE(result == Expression(200.));
+	}
+
+	{
+		std::string program = "(begin (define a 1) (if (<= a 0) 200 100))";
+		Expression result = run(program);
+		REQUIRE(result == Expression(100.));
+	}
+
+	{
+		std::string program = "(begin (define a 1) (if (<= a 1) 200 100))";
+		Expression result = run(program);
+		REQUIRE(result == Expression(200.));
+	}
+}
+//added cases
+TEST_CASE("Test Interpreter special forms: begin, define, if, and >", "[interpreter]") {
+
+	{
+		std::string program = "(begin (define a 3) (if (> a 2) 200 100))";
+		Expression result = run(program);
+		REQUIRE(result == Expression(200.));
+	}
+
+	{
+		std::string program = "(begin (define a 1) (if (> a 2) 200 100))";
+		Expression result = run(program);
+		REQUIRE(result == Expression(100.));
+	}
+}
+//added cases
+TEST_CASE("Test Interpreter special forms: begin, define, if, and >=", "[interpreter]") {
+
+	{
+		std::string program = "(begin (define a 1) (if (>= a 2) 200 100))";
+		Expression result = run(program);
+		REQUIRE(result == Expression(100.));
+	}
+
+	{
+		std::string program = "(begin (define a 1) (if (>= a 0) 200 100))";
+		Expression result = run(program);
+		REQUIRE(result == Expression(200.));
+	}
+
+	{
+		std::string program = "(begin (define a 1) (if (>= a 1) 200 100))";
+		Expression result = run(program);
+		REQUIRE(result == Expression(200.));
+	}
+}
+//added cases
+
 TEST_CASE( "Test a complex expression", "[interpreter]" ) {
 
   {
@@ -370,3 +502,55 @@ TEST_CASE( "Test all syntactically and semantically CORRECT files.", "[interpret
     REQUIRE(result == expected_result);
   }
 }
+
+/*
+TEST_CASE("Test Tokenize", "[tokenize]") {
+
+	std::string tokenInput = "( + 1 2 )";
+	std::istringstream iss(tokenInput);
+
+
+
+	std::vector<std::string> output = tokenizeInput(iss);
+	std::vector<std::string> tokens;
+	tokens.push_back("(");
+	tokens.push_back("+");
+	tokens.push_back("1");
+	tokens.push_back("2");
+	tokens.push_back(")");
+	REQUIRE(output == tokens);
+}
+
+TEST_CASE("Test Tokenize with \n", "[tokenize]") {
+
+	std::string tokenInput = "(\n+\n1 2\n)";
+	std::istringstream iss(tokenInput);
+
+
+
+	std::vector<std::string> output = tokenizeInput(iss);
+	std::vector<std::string> tokens;
+	tokens.push_back("(");
+	tokens.push_back("+");
+	tokens.push_back("1");
+	tokens.push_back("2");
+	tokens.push_back(")");
+	REQUIRE(output == tokens);
+}
+
+TEST_CASE("Test Tokenize", "[tokenize]") {
+
+	std::string tokenInput = "(\t+\t1\t2 )";
+	std::istringstream iss(tokenInput);
+
+
+
+	std::vector<std::string> output = tokenizeInput(iss);
+	std::vector<std::string> tokens;
+	tokens.push_back("(");
+	tokens.push_back("+");
+	tokens.push_back("1");
+	tokens.push_back("2");
+	tokens.push_back(")");
+	REQUIRE(output == tokens);
+}*/
--- unittests.cpp
+++ unittests.cpp
@@ -1,12 +1,136 @@
 #define CATCH_CONFIG_MAIN
 #define CATCH_CONFIG_COLOUR_NONE
 #include "catch.hpp"
-#include "tokenize.hpp"
+//#include "tokenize.hpp"
+//#include "expression.hpp"
+//#include "interpreter.hpp"
 
 ////////////////////////////////////
 //Additional Test Cases
+//More test cases were added to test_interpreter.cpp
+
+/*
+//added test case
+TEST_CASE("Test Interpreter parser with second half input", "[interpreter]") {
 
 
+{
+std::string program = ")";
+std::istringstream iss(program);
+
+Interpreter interp;
+bool ok = interp.parse(iss);
+REQUIRE(ok == false);
+}
+
+{
+std::string program = "f)";
+std::istringstream iss(program);
+
+Interpreter interp;
+bool ok = interp.parse(iss);
+REQUIRE(ok == false);
+}
+
+{
+std::string program = "gin (define r 10) (* pi (* r r)))";
+std::istringstream iss(program);
+
+Interpreter interp;
+bool ok = interp.parse(iss);
+REQUIRE(ok == false);
+}
+}
+
+//added cases
+TEST_CASE("Test Interpreter special forms: begin, define, and if", "[interpreter]") {
+
+{
+std::string program = "(begin (define a True) (if a 200 100))";
+Expression result = run(program);
+REQUIRE(result == Expression(200.));
+}
+
+{
+std::string program = "(begin (define a False) (if a 200 100))";
+Expression result = run(program);
+REQUIRE(result == Expression(100.));
+}
+}
+//added cases
+TEST_CASE("Test Interpreter special forms: begin, define, if, and <", "[interpreter]") {
+
+{
+std::string program = "(begin (define a 1) (if (< a 2) 200 100))";
+Expression result = run(program);
+REQUIRE(result == Expression(200.));
+}
+
+{
+std::string program = "(begin (define a 1) (if (< a 0) 200 100))";
+Expression result = run(program);
+REQUIRE(result == Expression(100.));
+}
+}
+
+//added cases
+TEST_CASE("Test Interpreter special forms: begin, define, if, and <=", "[interpreter]") {
+
+{
+std::string program = "(begin (define a 1) (if (<= a 2) 200 100))";
+Expression result = run(program);
+REQUIRE(result == Expression(200.));
+}
+
+{
+std::string program = "(begin (define a 1) (if (<= a 0) 200 100))";
+Expression result = run(program);
+REQUIRE(result == Expression(100.));
+}
+
+{
+std::string program = "(begin (define a 1) (if (<= a 1) 200 100))";
+Expression result = run(program);
+REQUIRE(result == Expression(200.));
+}
+}
+//added cases
+TEST_CASE("Test Interpreter special forms: begin, define, if, and >", "[interpreter]") {
+
+{
+std::string program = "(begin (define a 3) (if (> a 2) 200 100))";
+Expression result = run(program);
+REQUIRE(result == Expression(200.));
+}
+
+{
+std::string program = "(begin (define a 1) (if (> a 2) 200 100))";
+Expression result = run(program);
+REQUIRE(result == Expression(100.));
+}
+}
+//added cases
+TEST_CASE("Test Interpreter special forms: begin, define, if, and >=", "[interpreter]") {
+
+{
+std::string program = "(begin (define a 1) (if (>= a 2) 200 100))";
+Expression result = run(program);
+REQUIRE(result == Expression(100.));
+}
+
+{
+std::string program = "(begin (define a 1) (if (>= a 0) 200 100))";
+Expression result = run(program);
+REQUIRE(result == Expression(200.));
+}
+
+{
+std::string program = "(begin (define a 1) (if (>= a 1) 200 100))";
+Expression result = run(program);
+REQUIRE(result == Expression(200.));
+}
+}
+//added cases
 
 TEST_CASE("Test Tokenize", "[tokenize]") {
 
@@ -58,4 +182,4 @@ TEST_CASE("Test Tokenize", "[tokenize]") {
 	tokens.push_back(")");
 	REQUIRE(output == tokens);
 }
-
+*/
